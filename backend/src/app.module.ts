5938dca54866be8ed0516dd23809b880db154ce4
Brian Mwangi <brianmwangiat033@gmail.com>
2025-06-19 11:32:57 +0300

    [Added:]Prisma ORM, Module Cart and Module User

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, UserModule, AuthModule, CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
